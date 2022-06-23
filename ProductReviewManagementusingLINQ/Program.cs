65da1c37e803076ae785d20d1158dd98578b4b66
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-23 13:17:33 +0530

    Added to main

---
+++ ProductReviewManagementusingLINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagementusingLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
